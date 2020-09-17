75961911f1e050ed4a7b59e0413dc7e3b1254540
SeanMaker <43504934+SeanMaker@users.noreply.github.com>
2020-09-16 23:58:04 -0400

    Add files via upload

---
+++ HTML/DOM_exercise/index.js
@@ -0,0 +1,14 @@
+
+// const list_1=document.getElementById('li_1')
+// list_1.style.color='blue'
+//
+// const h1=document.getElementById('h1_1')
+// h1.style.backgroundColor='orange'
+//
+//
+// let li_s=document.getElementsByTagName('li')
+// li_s[1].style.backgroundColor='blue'
+
+const h1=document.querySelector('h1')
+console.log(h1)
+h1.style.backgroundColor='gray'
