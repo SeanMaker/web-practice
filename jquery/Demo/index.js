89b2397ad16fb540e1a04c1f433015b14bfa5a19
Shiheng Dong <dongshiheng9@gmail.com>
2020-09-24 23:59:00 -0400

    update

---
+++ jquery/Demo/index.js
@@ -0,0 +1,18 @@
+
+
+
+const styles={
+    width:'376px',
+    height:'282px'
+}
+
+$('img').css(styles)
+$('img:nth-of-type(1)').attr('src','https://cdn.akc.org/content/article-body-image/samoyed_puppy_dog_pictures.jpg')
+$('img').last().attr('src','https://cdn.akc.org/content/article-body-image/samoyed_puppy_dog_pictures.jpg')
+
+$('img').attr('src','https://c8.alamy.com/comp/MEP8KE/white-samoyed-puppy-dog-MEP8KE.jpg')
+
+$('li').addClass('correct')
+$('li').removeClass('correct')
+$('li').first().toggleClass('done')
+$('li').toggleClass('done')
